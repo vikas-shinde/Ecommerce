b2c4b9812d29552f25ab54ec03980119aada174a
Onkareshwar Koppal <onkarkoppal14@gmail.com>
2020-12-13 00:06:53 +0530

    Web API is committed

---
+++ WebApi/WebApi-Ecom/WebApi-Ecom/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Http;
+
+namespace WebApi_Ecom.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        public AccountController()
+        {
+           
+        }
+
+        [EnableCors()]
+        [Route("login")]
+        [HttpGet,AllowAnonymous]
+        public HttpResponseMessage Login(string UserName, string Password)
+        {
+
+            //HttpResponseMessage Request = new HttpResponseMessage();
+            HttpResponseMessage response = new HttpResponseMessage() { Content = new StringContent("Login Successfull"), StatusCode = HttpStatusCode.OK };
+       
+            return response;
+        }
+    }
+}
---
+++ WebApi/WebApi-Ecom/WebApi-Ecom/Startup.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WebApi_Ecom
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(
+                    builder =>
+                    {
+                        builder.WithOrigins("http://localhost:4200/");
+                    });
+            });
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseCors(corsPolicyBuilder =>
+            corsPolicyBuilder.WithOrigins("http://localhost:4200")
+           .AllowAnyMethod()
+           .AllowAnyHeader()
+           );
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
